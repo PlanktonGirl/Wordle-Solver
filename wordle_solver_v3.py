62a3004d51a17663a251fa191481e93c9d0f37e7
RebeccaSchwab <92683012+RebeccaSchwab@users.noreply.github.com>
2024-04-20 15:53:45 -0400

    Remove wrong_position global variable

--- wordle_solver_v3.py
+++ wordle_solver_v3.py
@@ -18,11 +18,7 @@ with open('./answers.txt') as answers_file:
 
 with open('./allowed.txt') as allowed_file:
     allowed_guesses = allowed_file.read().split()
-
-# The next 4 variables will store the inputs: 
-
-wrong_position = []
-                  
+                
 
 # This is a helper function for the yes or no inputs
 def helper_yn(question):
@@ -252,6 +248,7 @@ def get_known(eliminated):
 #This function let you input the correct and incorrect position of the known letters
 def get_positions(eliminated, known_letters):
     known_position = []
+    wrong_position = []
     print("Finally, I'm going to have you enter the positions of the letters you know.")
     print("")
     time.sleep(a)
@@ -339,10 +336,10 @@ def get_positions(eliminated, known_letters):
             time.sleep(a)
                        
     #runs the solve_wordle function 
-    solve_wordle(eliminated, known_letters, known_position)
+    solve_wordle(eliminated, known_letters, known_position, wrong_position)
 
 # This is the linear search function that checks the word lists againt the inputted letters:
-def solve_wordle(eliminated, known_letters, known_position):
+def solve_wordle(eliminated, known_letters, known_position, wrong_position):
     possible_words = set()
     # This code combines the two word lists and shuffles them so the answers list is mixed in with the allowed guesss list
     full_word_list = allowed_guesses + wordle_answers
